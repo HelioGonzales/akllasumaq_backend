9260f9af20f572a28308ee745a908142ad61d5b2
HelioGonzales <heliogonzalesr@gmail.com>
2023-09-28 22:07:30 +0200

    feata: start user auth

--- routers/users.js
+++ routers/users.js
@@ -1,11 +1,13 @@
 import express from "express";
+import bcrypt from "bcryptjs";
+import Jwt from "jsonwebtoken";
 import { User } from "../models/user.js";
 
 export const routerUser = express.Router();
 
 /* Route */
 routerUser.get(`/`, async (req, res) => {
-  const userList = await User.find();
+  const userList = await User.find().select("-passwordHash");
 
   if (!userList) {
     res.status(500).json({
@@ -15,22 +17,98 @@ routerUser.get(`/`, async (req, res) => {
   res.send(userList);
 });
 
-routerUser.post(`/`, (req, res) => {
-  const user = new User({
+routerUser.get(`/:id`, async (req, res) => {
+  const user = await User.findById(req.params.id).select("-passwordHash");
+
+  if (!user) {
+    res.status(500).json({
+      message: "The user with the given ID was not found",
+    });
+  }
+  res.status(200).send(user);
+});
+
+routerUser.put("/:id", async (req, res) => {
+  const userExist = await User.findById(req.params.id);
+  let newPassword;
+  if (req.body.password) {
+    newPassword = bcrypt.hashSync(req.body.password, 10);
+  } else {
+    newPassword = userExist.passwordHash;
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      passwordHash: newPassword,
+      phone: req.body.phone,
+      isAdmin: req.body.isAdmin,
+      street: req.body.street,
+      apartment: req.body.apartment,
+      zip: req.body.zip,
+      city: req.body.city,
+      country: req.body.country,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!user) {
+    return res.status(404).send("the user cannot be updated");
+  }
+
+  res.send(user);
+});
+
+routerUser.post(`/`, async (req, res) => {
+  let user = new User({
     name: req.body.name,
-    image: req.body.image,
-    countInStock: req.body.countInStock,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(`${req.body.password}`, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
   });
 
-  user
-    .save()
-    .then((createdUser) => {
-      res.status(201).json(createdUser);
-    })
-    .catch((err) => {
-      res.status(501).json({
-        error: err,
-        success: false,
-      });
-    });
+  user = await user.save();
+
+  if (!user) {
+    return res.status(400).send("The user cannot be created");
+  }
+
+  res.send(user);
+});
+
+routerUser.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  const secret = process.env.SECRET;
+  if (!user) {
+    return res.status(400).send("User not found");
+  }
+
+  if (
+    user &&
+    bcrypt.compareSync(`${req.body.password}`, `${user.passwordHash}`)
+  ) {
+    const token = Jwt.sign(
+      {
+        userId: user.id,
+      },
+      secret,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    res.status(200).send({ user: user.email, token: token });
+  } else {
+    return res.status(400).send("Password is wrong");
+  }
 });
