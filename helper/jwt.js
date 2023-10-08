756f66a9a1f7d95654f68cf4a6d79f928e3c6df4
HelioGonzales <heliogonzalesr@gmail.com>
2023-10-08 21:39:47 +0200

    feat: allow api without auth cus front dev

--- helper/jwt.js
+++ helper/jwt.js
@@ -9,11 +9,13 @@ export function authJwt() {
     algorithms: ["HS256"],
   }).unless({
     path: [
-      { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
-      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
-      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
-      "/api/v1/users/login",
-      "/api/v1/users/register",
+      // { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
+      // { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      // { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      // { url: /\/api\/v1\/orders(.*)/, methods: ["GET", "OPTIONS"] },
+      // "/api/v1/users/login",
+      // "/api/v1/users/register",
+      { url: /(.*)/ },
     ],
   });
 }
