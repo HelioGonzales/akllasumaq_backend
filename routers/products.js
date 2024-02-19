464dd2d12f17e0d52a8f2ad7ef619967a5c6cb79
HelioGonzales <heliogonzalesr@gmail.com>
2024-02-19 22:21:25 +0100

    feat: implement img storage in AWS S3 + CRUD

--- routers/products.js
+++ routers/products.js
@@ -3,34 +3,46 @@ import mongoose from "mongoose";
 import multer from "multer";
 import { Product } from "../models/product.js";
 import { Category } from "../models/category.js";
+import sharp from "sharp";
+import { s3Commands } from "../helper/s3Helper.js";
+import * as fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const routerProduct = express.Router();
 
-const FILE_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpeg",
-  "image/jpg": "jpg",
-};
+const s3BaseUrl = `https://akllasumaq.s3.eu-north-1.amazonaws.com/`;
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const isValid = FILE_TYPE_MAP[file.mimetype];
-    let uploadError = new Error("Invalid image type");
+    const uploadDir = "public/uploads";
 
-    if (isValid) {
-      uploadError = null;
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
     }
-
-    cb(uploadError, "public/uploads");
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
-    const fileName = file.originalname.split(" ").join("-");
-    const extension = FILE_TYPE_MAP[file.mimetype];
-    cb(null, `${fileName}-${Date.now()}.${extension}`);
+    const originalName = file.originalname;
+    const extension = originalName.split(".").pop();
+    cb(null, Date.now() + "." + extension);
   },
 });
 
-const uploadOptions = multer({ storage: storage });
+const uploadOptions = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only image files are allowed."));
+    }
+  },
+});
 
 /* Route */
 routerProduct.get(`/`, async (req, res) => {
@@ -73,42 +85,78 @@ routerProduct.put("/:id", uploadOptions.single("image"), async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (!product) return res.status(400).send("Invalid Product");
 
-  const file = req.file;
-  let imagesPath;
+  let imagesPath = product.image;
+  let processedImageBuffer; // Define processedImageBuffer in a broader scope
 
-  if (file) {
-    const fileName = req.file.filename;
-    const basePath = `https://${req.get("host")}/public/uploads/`;
-    imagesPath = `${basePath}${fileName}`;
-  } else {
-    imagesPath = product.image;
-  }
+  try {
+    const file = req.file;
 
-  const updatedProduct = await Product.findByIdAndUpdate(
-    req.params.id,
-    {
-      name: req.body.name,
-      description: req.body.description,
-      richDescription: req.body.richDescription,
-      image: imagesPath,
-      brand: req.body.brand,
-      price: req.body.price,
-      category: req.body.category,
-      countInStock: req.body.countInStock,
-      rating: req.body.rating,
-      numReviews: req.body.numReviews,
-      isFeatured: req.body.isFeatured,
-    },
-    {
-      new: true,
+    if (file) {
+      const fileName = req.file.filename;
+      const basePath = `${s3BaseUrl}${file.path}`;
+
+      // Use Sharp to process the image (e.g., rotate) before saving to S3
+      processedImageBuffer = await sharp(file.path).rotate().toBuffer();
+
+      // Save processed image to S3
+      await s3Commands.addObject(fileName, processedImageBuffer);
+
+      imagesPath = basePath.includes("public\\uploads\\")
+        ? basePath.replace("public\\uploads\\", "")
+        : basePath.includes("public/uploads/")
+        ? basePath.replace("public/uploads/", "")
+        : basePath;
     }
-  );
 
-  if (!updatedProduct) {
-    return res.status(404).send("the product cannot be updated");
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+        image: imagesPath,
+      },
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).send("The product cannot be updated");
+    }
+
+    // Delete the old image from AWS S3
+    if (file && product.image) {
+      const oldImageKey = product.image.split("/").pop(); // Extract filename from the old image path
+      await s3Commands.deleteObject(oldImageKey);
+    }
+
+    // Delete the local image file
+    if (product.image) {
+      const localImagePath = `public/uploads/${product.image.split("/").pop()}`;
+      if (fs.existsSync(localImagePath)) {
+        fs.unlinkSync(localImagePath);
+      }
+    }
+
+    // Save the new image to local file system
+    if (file) {
+      const newLocalImagePath = `public/uploads/${file.filename}`;
+      fs.writeFileSync(newLocalImagePath, processedImageBuffer);
+    }
+
+    res.send(updatedProduct);
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).send("Internal server error");
   }
-
-  res.send(product);
 });
 
 routerProduct.post(`/`, uploadOptions.single("image"), async (req, res) => {
@@ -123,48 +171,82 @@ routerProduct.post(`/`, uploadOptions.single("image"), async (req, res) => {
   }
 
   const fileName = req.file.filename;
-  const basePath = `https://${req.get("host")}/public/uploads/`;
+  const basePath = `${s3BaseUrl}${file.path}`;
 
-  let product = await new Product({
-    name: req.body.name,
-    description: req.body.description,
-    richDescription: req.body.richDescription,
-    image: `${basePath}${fileName}`,
-    brand: req.body.brand,
-    price: req.body.price,
-    category: req.body.category,
-    countInStock: req.body.countInStock,
-    rating: req.body.rating,
-    numReviews: req.body.numReviews,
-    isFeatured: req.body.isFeatured,
-  });
+  try {
+    // Use Sharp to process the image (e.g., rotate) before saving to S3
+    const processedImageBuffer = await sharp(file.path).rotate().toBuffer();
 
-  product = await product.save();
+    // Save processed image to S3
+    await s3Commands.addObject(fileName, processedImageBuffer);
 
-  if (!product) {
-    return res.status(500).send("The product cannot be created");
+    let product = await new Product({
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
+      image: basePath.includes("public\\uploads\\")
+        ? basePath.replace("public\\uploads\\", "")
+        : basePath.includes("public/uploads/")
+        ? basePath.replace("public/uploads/", "")
+        : basePath,
+    });
+
+    product = await product.save();
+
+    if (!product) {
+      return res.status(500).send("The product cannot be created");
+    }
+
+    res.send(product);
+  } catch (error) {
+    console.error("S3 Upload Error:", error);
+    return res.status(500).send("Internal server error");
   }
-
-  res.send(product);
 });
 
-routerProduct.delete("/:id", (req, res) => {
-  Product.findByIdAndRemove(req.params.id)
-    .then((product) => {
-      if (product) {
-        return res.status(200).json({
-          success: true,
-          message: "Product is deleted",
-        });
-      } else {
-        return res
-          .status(404)
-          .json({ success: false, message: "Product not found" });
+routerProduct.delete("/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndRemove(req.params.id);
+
+    if (product) {
+      // Delete the image from AWS S3
+      if (product.image) {
+        const imageKey = product.image.split("/").pop(); // Extract filename from the image path
+        await s3Commands.deleteObject(imageKey);
       }
-    })
-    .catch((err) => {
-      return res.status(400).json({ success: false, error: err });
-    });
+
+      // Delete the local image file
+      if (product.image) {
+        const localImagePath = `public/uploads/${product.image
+          .split("/")
+          .pop()}`;
+        if (fs.existsSync(localImagePath)) {
+          fs.unlinkSync(localImagePath);
+        }
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: "Product and associated image are deleted",
+      });
+    } else {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+  } catch (err) {
+    console.error("Error:", err);
+    return res
+      .status(500)
+      .json({ success: false, error: "Internal server error" });
+  }
 });
 
 routerProduct.get("/get/count", async (req, res) => {
